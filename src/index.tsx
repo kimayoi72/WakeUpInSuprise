a0e24f50f636327e4b390795e8db9507746bdd28
Frank Lindecke <frank.lindecke@googlemail.com>
2018-11-10 16:01:47 +0100

    Use the service worker

--- src/index.tsx
+++ src/index.tsx
@@ -9,4 +9,8 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register({ 
+  onUpdate : (registration:any) => console.log('Registration update', registration),
+  onSuccess: (registration:any) => console.log('Registration success', registration),
+
+});
