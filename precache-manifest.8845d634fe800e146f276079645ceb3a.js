cc75a2d8aea75ce9602d3c00d9843f83706b7f65
Frank Lindecke <frank.lindecke@googlemail.com>
2018-11-10 16:23:54 +0100

    Updates

---
+++ precache-manifest.8845d634fe800e146f276079645ceb3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f96a7998d28ab712166a",
+    "url": "/wake-up-in-surprise/static/css/main.4ea44268.chunk.css"
+  },
+  {
+    "revision": "f96a7998d28ab712166a",
+    "url": "/wake-up-in-surprise/static/js/main.f96a7998.chunk.js"
+  },
+  {
+    "revision": "7bc88e323dfded01db29",
+    "url": "/wake-up-in-surprise/static/js/1.7bc88e32.chunk.js"
+  },
+  {
+    "revision": "f61239cd0c941753256f",
+    "url": "/wake-up-in-surprise/static/js/runtime~main.f61239cd.js"
+  },
+  {
+    "revision": "3c7db145a0572a9ff667e16472bb42b6",
+    "url": "/wake-up-in-surprise/static/media/abstract-vortex-2-512-247726.3c7db145.png"
+  },
+  {
+    "revision": "de70d85a9aa70d29a524453780985af0",
+    "url": "/wake-up-in-surprise/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wake-up-in-surprise/precache-manifest.275bb412ecb98acb4b98f5cc5d9f152b.js"
+  "/wake-up-in-surprise/precache-manifest.8845d634fe800e146f276079645ceb3a.js"
 );
 
 workbox.clientsClaim();
