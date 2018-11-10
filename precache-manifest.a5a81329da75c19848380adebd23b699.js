1a959326207efac1f06ab1e54aca2f4230b3fb85
Frank Lindecke <frank.lindecke@googlemail.com>
2018-11-10 12:28:05 +0100

    Updates

---
+++ precache-manifest.a5a81329da75c19848380adebd23b699.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75eeeba7eb4d9374f935",
+    "url": "/kimayoi72/wake-up-in-surprise/static/css/main.c1f0f8db.chunk.css"
+  },
+  {
+    "revision": "75eeeba7eb4d9374f935",
+    "url": "/kimayoi72/wake-up-in-surprise/static/js/main.75eeeba7.chunk.js"
+  },
+  {
+    "revision": "99c24283125b9a51913f",
+    "url": "/kimayoi72/wake-up-in-surprise/static/js/1.99c24283.chunk.js"
+  },
+  {
+    "revision": "6930414715783f05640f",
+    "url": "/kimayoi72/wake-up-in-surprise/static/js/runtime~main.69304147.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kimayoi72/wake-up-in-surprise/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7b736ea1e5f5cacc41685fcb484971cc",
+    "url": "/kimayoi72/wake-up-in-surprise/index.html"
+  }
+];
