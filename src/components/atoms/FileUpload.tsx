7627d749c49f105fb8755063b00d99622158a47d
Frank Lindecke <frank.lindecke@googlemail.com>
2018-11-11 15:21:54 +0100

    Moved file upload into its own component

---
+++ src/components/atoms/FileUpload.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import Dropzone from "react-dropzone";
+
+export type FileContent = string | ArrayBuffer;
+
+export interface FileUploadProps {
+  onFileLoaded?: (file: File, content: FileContent) => void;
+  accept?: string;
+}
+
+const FileUpload = ({ onFileLoaded, accept = "audio/*" }: FileUploadProps) => {
+  const defaultState: File[] = [];
+  const [files, setFiles] = useState(defaultState);
+  const [uploadedFiles, setUploadedFiles] = useState(defaultState);
+
+  useEffect(
+    () => {
+      let localFileList: File[] = [];
+      files.forEach(file => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          if (reader.result === null) {
+            return;
+          }
+          onFileLoaded && onFileLoaded(file, reader.result);
+          localFileList = localFileList.concat(file);
+          setUploadedFiles(localFileList);
+        };
+        reader.onabort = () => console.log("file reading was aborted");
+        reader.onerror = () => console.log("file reading has failed");
+        reader.readAsDataURL(file);
+      });
+    },
+    [files]
+  );
+
+  const onDrop = (acceptedFiles: File[]) => {
+    setUploadedFiles([]);
+    setFiles(acceptedFiles);
+  };
+
+  let css : React.CSSProperties = {
+    position: "relative",
+    margin: "15px",
+    width: "calc(100%-30px)",
+    height: "calc(100%-30px)",
+    minHeight: "200px",
+    borderWidth: "2px",
+    borderColor: "rgb(102, 102, 102)",
+    borderStyle: "dashed",
+    borderRadius: "5px",
+    textAlign: "left",
+  }
+  
+  return (
+    <Dropzone onDrop={onDrop} accept={accept} style={css}>
+      <ul>
+        {uploadedFiles.map(file => (
+          <li key={file.name}>
+            {file.name} - {file.size}
+          </li>
+        ))}
+      </ul>
+    </Dropzone>
+  );
+};
+
+export default FileUpload;
--- src/components/pages/Upload.tsx
+++ src/components/pages/Upload.tsx
@@ -1,65 +1,15 @@
-import React, { useState, useEffect } from "react";
-import Dropzone from "react-dropzone";
+import React from 'react'
 
-export type FileContent = string | ArrayBuffer;
+import FileUpload, { FileContent } from "../atoms/FileUpload";
 
-export interface UploadProps {
-  onFileLoaded?: (file: File, content: FileContent) => void;
-}
-
-const Upload = ({ onFileLoaded }: UploadProps) => {
-  const defaultState: File[] = [];
-  const [files, setFiles] = useState(defaultState);
-  const [uploadedFiles, setUploadedFiles] = useState(defaultState);
-
-  useEffect(
-    () => {
-      let localFileList: File[] = [];
-      files.forEach(file => {
-        const reader = new FileReader();
-        reader.onload = () => {
-          if (reader.result === null) {
-            return;
-          }
-          onFileLoaded && onFileLoaded(file, reader.result);
-          localFileList = localFileList.concat(file);
-          setUploadedFiles(localFileList);
-        };
-        reader.onabort = () => console.log("file reading was aborted");
-        reader.onerror = () => console.log("file reading has failed");
-        reader.readAsDataURL(file);
-      });
-    },
-    [files]
-  );
-
-  const onDrop = (acceptedFiles: File[]) => {
-    setUploadedFiles([]);
-    setFiles(acceptedFiles);
-  };
-
-  return (
-    <Dropzone onDrop={onDrop}>
-      <div>Label</div>
-      <ul>
-        {uploadedFiles.map(file => (
-          <li key={file.name}>
-            {file.name} - {file.size}
-          </li>
-        ))}
-      </ul>
-    </Dropzone>
-  );
-};
-
-const UploadPage = () => {
+const Upload = () => {
   const onFileLoaded = (file: File, content: FileContent) => {
     console.log(file.name, file.size, content.toString().length);
   };
   return (
     <div>
       <h1>Upload</h1>
-      <Upload onFileLoaded={onFileLoaded} />
+      <FileUpload onFileLoaded={onFileLoaded} />
     </div>
   );
 };
