2419901df95cff6f85576b6e88a8445d1447af06
Frank Lindecke <frank.lindecke@googlemail.com>
2019-01-08 21:44:39 +0100

    Removed unused imports of AudioPlayer

--- src/components/atoms/AudioPlayer.tsx
+++ src/components/atoms/AudioPlayer.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, Component } from "react";
-import { render } from "react-dom";
+import React, { Component } from "react";
 
 interface AudioPlayerProps {
   url: string;
