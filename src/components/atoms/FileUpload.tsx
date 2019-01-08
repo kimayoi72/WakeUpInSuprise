41747c7bd2b4af5a7600b291a35bcca5d3c0cb64
Frank Lindecke <frank.lindecke@googlemail.com>
2019-01-08 21:44:53 +0100

    Use class implementation of FileUpload

--- src/components/atoms/FileUpload.tsx
+++ src/components/atoms/FileUpload.tsx
@@ -1,70 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import Dropzone from "react-dropzone";
 
 export type FileContent = string | ArrayBuffer;
 
 export interface FileUploadProps {
   onFileLoaded?: (file: File, content: FileContent) => void;
-  accept?: string;
+  accept?: string
 }
 
-const FileUpload = ({ onFileLoaded, accept = "audio/*" }: FileUploadProps) => {
-  const defaultState: File[] = [];
-  const [files, setFiles] = useState(defaultState);
-  const [uploadedFiles, setUploadedFiles] = useState(defaultState);
+interface FileUploadState {
+  files: File[];
+  uploadedFiles: File[];
+}
 
-  useEffect(
-    () => {
+const css: React.CSSProperties = {
+  position: "relative",
+  margin: "15px",
+  width: "calc(100%-30px)",
+  height: "calc(100%-30px)",
+  minHeight: "200px",
+  borderWidth: "2px",
+  borderColor: "rgb(102, 102, 102)",
+  borderStyle: "dashed",
+  borderRadius: "5px",
+  textAlign: "left"
+};
+
+class FileUpload extends Component<FileUploadProps, FileUploadState> {
+  constructor(props: Readonly<FileUploadProps>) {
+    super(props);
+    this.state = {
+      files: [],
+      uploadedFiles: []
+    }
+  }
+
+  onDrop = (acceptedFiles: File[]) => {
+    this.setState({
+      uploadedFiles: [],
+      files: acceptedFiles
+    }, () => {
       let localFileList: File[] = [];
-      files.forEach(file => {
-        console.log(file)
-
+      this.state.files.forEach(file => {
         const reader = new FileReader()
         reader.onload = () => {
-          if (reader.result === null) {
-            return;
-          }
-          onFileLoaded && onFileLoaded(file, reader.result);
+          if (reader.result === null) return;
+          this.props.onFileLoaded && this.props.onFileLoaded(file, reader.result);
           localFileList = localFileList.concat(file);
-          setUploadedFiles(localFileList);
+          this.setState({ uploadedFiles: localFileList})
         };
         reader.onabort = () => console.log("file reading was aborted");
         reader.onerror = () => console.log("file reading has failed");
         reader.readAsDataURL(file);
       });
-    },
-    [files]
-  );
-
-  const onDrop = (acceptedFiles: File[]) => {
-    setUploadedFiles([]);
-    setFiles(acceptedFiles);
+    })
   };
 
-  let css : React.CSSProperties = {
-    position: "relative",
-    margin: "15px",
-    width: "calc(100%-30px)",
-    height: "calc(100%-30px)",
-    minHeight: "200px",
-    borderWidth: "2px",
-    borderColor: "rgb(102, 102, 102)",
-    borderStyle: "dashed",
-    borderRadius: "5px",
-    textAlign: "left",
+  public render() {
+    return (
+      <Dropzone onDrop={this.onDrop} accept={this.props.accept} style={css}>
+        <ul>
+          {this.state.uploadedFiles.map(file => (
+            <li key={file.name}>
+              {file.name} - {file.size}
+            </li>
+          ))}
+        </ul>
+      </Dropzone>
+    );
   }
-
-  return (
-    <Dropzone onDrop={onDrop} accept={accept} style={css}>
-      <ul>
-        {uploadedFiles.map(file => (
-          <li key={file.name}>
-            {file.name} - {file.size}
-          </li>
-        ))}
-      </ul>
-    </Dropzone>
-  );
-};
+}
 
 export default FileUpload;
